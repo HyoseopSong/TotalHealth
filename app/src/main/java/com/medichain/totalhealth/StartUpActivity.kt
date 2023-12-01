b4d7ee173388639756b15775bbf61ee31e9f6e92
송효섭 <csnvshs@naver.com>
2023-12-01 20:22:46 +0900

    Firebase 클라우드 메시징에서 앱 푸시 테스트 성공

--- app/src/main/java/com/medichain/totalhealth/StartUpActivity.kt
+++ app/src/main/java/com/medichain/totalhealth/StartUpActivity.kt
@@ -1,16 +1,25 @@
 package com.medichain.totalhealth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
 import com.medichain.totalhealth.databinding.ActivityStartUpBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
 
 class StartUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStartUpBinding
@@ -21,6 +30,23 @@ class StartUpActivity : AppCompatActivity() {
         binding.isSplash = true
         binding.activity = this@StartUpActivity
 
+        permissionCheck()
+
+        // 현재 토큰을 가져오려면
+        // FirebaseMessaging.getInstace().getToken()을 호출한다.
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("MyTAG_FCM", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // FCM 등록 토큰 가져오기
+            val token = task.result
+
+            val msg = "FCM Registration token: " + token;
+            Log.d("MyTAG_FCM", msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
         CoroutineScope(Dispatchers.Default).launch {
             delay(3000)
             binding.isSplash = false
@@ -33,5 +59,42 @@ class StartUpActivity : AppCompatActivity() {
 
         startActivity(mIntent)
         finish()
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(applicationContext, "Permission is denied", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(applicationContext, "Permission is granted", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+
+    private val PERMISSION_REQUEST_CODE = 5000
+    private fun permissionCheck() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val permissionCheck = ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.POST_NOTIFICATIONS
+            )
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                    PERMISSION_REQUEST_CODE
+                )
+            }
+        }
     }
 }
